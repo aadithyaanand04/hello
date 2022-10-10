739bda3461321e47254d7b656de20e1906d0dacc
aadithya anand <114949323+aadithyaanand04@users.noreply.github.com>
2022-10-10 21:22:08 +0530

    Create program 36.c

---
+++ program 36.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int x = 5;
+  int y = 3;
+  
+  // Returns 0 (false) because 5 is neither less than or equal to 3
+  printf("%d", x <= y);
+  return 0;
+}
+Footer
