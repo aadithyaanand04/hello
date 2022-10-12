4974463c2bf398515204633055238a8c4d3eec59
aadithya anand <114949323+aadithyaanand04@users.noreply.github.com>
2022-10-12 10:25:36 +0530

    Update program 14.c

--- program 14.c
+++ program 14.c
@@ -1,7 +1,8 @@
 c#include <stdio.h>
 
 int main() {
-  int sum1 = 100 + 50;        
+ printf("RA2211042010060\n");
+ int sum1 = 100 + 50;        
   int sum2 = sum1 + 250;      
   int sum3 = sum2 + sum2;     
   printf("%d\n", sum1);
