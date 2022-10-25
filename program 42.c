4d1f149b16828f9b08201749d8ec0619555d58fa
aadithya anand <114949323+aadithyaanand04@users.noreply.github.com>
2022-10-25 22:04:58 +0530

    Create program 42.c

---
+++ program 42.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+printf("Register number: RA2211042010060\n");
+int time = 20;
+if (time < 18)
+{
+  printf("Good day.");
+} 
+else
+{
+  printf("Good evening.");
+}
+}
