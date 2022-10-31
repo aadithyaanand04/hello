37890f99b4f63b5f7997cda994abd14ba7a63e38
aadithya anand <114949323+aadithyaanand04@users.noreply.github.com>
2022-10-31 21:08:34 +0530

    Create program 44.c

---
+++ program 44.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+  printf("Register number: RA2211042010060\n");
+  int myNum = 10;
+  
+  if (myNum > 0)
+  {
+    printf("The value is a positive number.");
+  }
+  else if (myNum < 0)
+  {
+    printf("The value is a negative number.");
+  }
+  else
+  {
+    printf("The value is 0.");
+  } 
+}
