18194958c5ab45d3491e885b5c3774822d9a2417
aadithya anand <114949323+aadithyaanand04@users.noreply.github.com>
2022-10-31 21:10:09 +0530

    Create program 46.c

---
+++ program 46.c
@@ -0,0 +1,19 @@
+//voting program 
+#include <stdio.h>
+
+int main()
+{
+ printf("Register number: RA2211042010060\n");
+ int age;
+ printf("enter your age\n");
+ scanf("%d",&age);
+ if(age >=18)
+ {
+     printf("can vote");
+ }
+ else
+ {
+     printf("cannot vote");
+ }
+  
+}
