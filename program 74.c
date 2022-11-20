8eacf5549842ef0d1374a2def2c70b7cc04ae788
aadithya anand <114949323+aadithyaanand04@users.noreply.github.com>
2022-11-20 22:45:49 +0530

    Create program 74.c

---
+++ program 74.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Register number: RA2211042010060\n\n");
+    float length;
+    float breadth;
+    float area;
+    float perimeter;
+    printf("Enter the length of the rectangle: ");
+    scanf("%f",&length);
+    printf("Enter the breadth of the rectangle: ");
+    scanf("%f",&breadth);
+    
+    area = length*breadth;
+    perimeter = 2*length+2*breadth;
+    
+    printf("The area of the rectangle is: %f\n",area);
+    printf("The perimeter of the rectangle is: %f\n",perimeter);
+    
+    
+     return 0;
+}
+Footer
