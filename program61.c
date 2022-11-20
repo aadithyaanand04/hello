475346d1dfe476e4fad010704b723def1237009a
aadithya anand <114949323+aadithyaanand04@users.noreply.github.com>
2022-11-20 22:15:30 +0530

    Create program61.c

---
+++ program61.c
@@ -0,0 +1,13 @@
+//take number from user and add all the preceding numbers
+#include <stdio.h>
+int main()
+{
+ int t,sum_n=0;
+ scanf("%d",&t);
+ for(int i=1;i<=t;i++)
+ {
+     sum_n = sum_n +i;
+ }
+ printf("%d",sum_n);
+    return 0;
+}
