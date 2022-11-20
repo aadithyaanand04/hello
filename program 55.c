c6219cf167b22c488ff922613aca56bef520dfd6
aadithya anand <114949323+aadithyaanand04@users.noreply.github.com>
2022-11-20 22:06:04 +0530

    Create program 55.c

---
+++ program 55.c
@@ -0,0 +1,18 @@
+#include <stdio.h>  
+int main()  
+{   
+    printf("Register number: RA2211042010060\n");
+    int x = 10, y = 5;   
+    switch(x>y && x+y>0)  
+    {  
+        case 1:   
+        printf("hi");  
+        break;   
+        case 0:   
+        printf("bye");  
+        break;  
+        default:   
+        printf(" Hello bye ");  
+    }   
+          
+}  
