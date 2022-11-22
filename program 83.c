a70e855c9fe8202902f02879e87a82d27c5bb8d3
aadithya anand <114949323+aadithyaanand04@users.noreply.github.com>
2022-11-22 08:21:25 +0530

    Create program 83.c

---
+++ program 83.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  prinf("Register number: RA2211042010060\n\n");
+  float a,b;
+  scanf("%f",&a);
+  scanf("%f",&b);
+  printf("%0.6f\n",a);
+  printf("%0.6f\n",b);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
