c01ecf5123a62f404e5326fc3b392b148a5df09a
aadithya anand <114949323+aadithyaanand04@users.noreply.github.com>
2022-11-22 08:30:17 +0530

    Create program 90.c

---
+++ program 90.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Register number: RA2211042010060\n\n");
+    float a,b,c;
+    scanf("%f %f",&a,&b);
+    c = a*b;
+    printf("%0.4f",c);
+
+    return 0;
+}
+Footer
