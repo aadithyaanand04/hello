a87032cf995c2ccccf2c2b0f3aaa5217c52e9ae9
aadithya anand <114949323+aadithyaanand04@users.noreply.github.com>
2022-11-22 08:35:19 +0530

    Create program 94.c

---
+++ program 94.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    float odd;
+    scanf("%d",&N);
+    if (N%2==0)
+    {printf("0");}
+    else{
+        odd =N/2+1;
+        printf("%0.9f",odd/N);
+    }
+
+    return 0;
+}
+Footer
